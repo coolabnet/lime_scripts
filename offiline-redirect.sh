64b4bf256b135d99aa068171877f25f0c2a9d1c2
bruno vianna <bruno@pobox.com>
2018-03-06 15:55:32 -0300

    updated comments

--- offiline-redirect.sh
+++ offiline-redirect.sh
@@ -5,6 +5,11 @@
 #the local server address is assumed to be 10.x.128.100 (change it as you like)
 #the script looks up the local subnet (10.??.x.x)
 
+#it is a good idea to change the default setting subnet of /etc/config/lime to 	
+#   option main_ipv4_address '10.%N1.0.0/17'
+#(17 instead of 16)
+#this way, only addresses up to 10.x.127.254 will be distributed by dhcp and you can use ips above 10.x.128.0 for static addresses
+
 if ! ip route | grep -q -e default # if there is no route out, then...
 then
 #  offline; check if redirect is already there
